7b8b568b781dea574ef1850ee94fe57ebaf8fede
ameliawht75 <ameliawht75@gmail.com>
2025-04-29 17:35:17 -0600

    upload 2

--- src/components/CartList.tsx
+++ src/components/CartList.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { CartItem, Game } from '../types';
 import CartItemRow from './CartItemRow';
 
@@ -8,25 +8,43 @@ type Props = {
     games: Game[]
 }
 export default function CartList({ cartItems, setCartItems, games }: Props) {
+    const [loading, setLoading] = useState(false) //Loading indicator so user knows something is happening.
+    const [errorMessage, setErrorMessage] = useState("")
+
     useEffect(() => {
         const asyncFunction = async () => {
-            const response = await fetch('http://localhost:3000/cart')
-            const data = await response.json()
-            setCartItems(data)
+            setLoading(true)
+            try {
+                const response = await fetch('http://localhost:3000/cart')
+                if (!response.ok) {
+                    setErrorMessage(response.statusText)
+                } else {
+                    const data = await response.json()
+                    setCartItems(data)
+                }
+            } catch (error: any) {
+                setErrorMessage(error.message)
+            }
+            setLoading(false)
         }
         asyncFunction()
     }, [])
     return (
-        <table className="table table-striped">
-            <tbody>
-                {cartItems.map(item => (
-                    <CartItemRow 
-                    key={item.id}
-                    item={item} 
-                    games={games}
-                    />
-                ))}
-            </tbody>
-        </table>
+        <>
+            {loading ? <p className="text-body-tertiary">Loading...</p> :
+                errorMessage ? <p className="text-danger">{errorMessage}</p> :
+                    <table className="table table-striped">
+                        <tbody>
+                            {cartItems.map(item => (
+                                <CartItemRow
+                                    key={item.id}
+                                    item={item}
+                                    games={games}
+                                />
+                            ))}
+                        </tbody>
+                    </table>
+            }
+        </>
     )
 }
--- src/components/GameList.tsx
+++ src/components/GameList.tsx
@@ -1,60 +1,89 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import type { CartItem, Game } from '../types'
 
 type Props = {
-    cartItems: CartItem[],
-    setCartItems: (newValue: CartItem[]) => void
-    games: Game[],
-    setGames: (newValue: Game[]) => void
+  cartItems: CartItem[],
+  setCartItems: (newValue: CartItem[]) => void
+  games: Game[],
+  setGames: (newValue: Game[]) => void
 }
 
-    export default function GameList({
-        setCartItems, cartItems, games, setGames
-    }: Props) {
+export default function GameList({
+  setCartItems, cartItems, games, setGames
+}: Props) {
+  const [isLoading, setIsLoading] = useState(false) //Loading indicator
+  const [isAddingToCart, setIsAddingToCart] = useState(false) //Loading indicator for adding to cart
+  const [error, setError] = useState<null | string>(null) //Error handling
 
 
-        //after the first render, we want to go and get the data, 
-          // then render again with the data
-          useEffect(() => {
-            const asyncFunction = async () => {
-            const response = await fetch ('http://localhost:3000/games')
-            const data = await response.json()
-            setGames(data)
-            }
-            asyncFunction()
-          }, []) //run once after the first render and twice in dev mode
-        
-
-        const addToCart = async (gameId: number) => {
-            const newCartItem = {
-              gameId: gameId, 
-              amount: 1
-            }
-            //make the change on the backend
-            const response = await fetch('http://localhost:3000/cart', {
-              method: 'POST',
-              body: JSON.stringify(newCartItem),
-              headers: {
-                "Content-Type": "application/json",
-              },
-            })
-            const newlyCreatedItem = await response.json() //this will have an id on it 
-            //make the change on the front end
-            setCartItems([...cartItems, newlyCreatedItem])
-          }
-        return (
-            <div className="d-flex flex-wrap gap-3">
-            {games.map(game => (
-              <div key={game.id} className="card flex-grow-1">
-                <h3 className="card-title">{game.title}</h3>
-                <p className="card-text">{game.genre}</p>
-                <button className="btn btn-primary" 
-                onClick={() => addToCart(game.id)}
-                >
-                  ${game.price.toFixed(2)}
-                  </button>
-              </div>
-            ))}
-            </div>
-        )
+  //after the first render, we want to go and get the data, 
+  // then render again with the data
+  useEffect(() => {
+    const asyncFunction = async () => {
+      setIsLoading(true)
+      try {
+        const response = await fetch('http://localhost:3000/games')
+        if (!response.ok) {
+          setError("There was an error fetching the data: " + response.statusText)
+        } else {
+          const data = await response.json()
+          setGames(data)
+          setError(null) //clear the error if we get data
+        }
+      } catch (error: any) { //tells typescript not to worry about the type of error
+        setError("There was an error fetching the data: " + error.message)
+      }
+      setIsLoading(false)
     }
+    asyncFunction()
+  }, []) //run once after the first render and twice in dev mode
+
+
+  const addToCart = async (gameId: number) => {
+    const newCartItem = {
+      gameId: gameId,
+      amount: 1
+    }
+    //make the change on the backend
+    setIsAddingToCart(true)
+    try {
+      const response = await fetch('http://localhost:3000/cart', {
+        method: 'POST',
+        body: JSON.stringify(newCartItem),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      if (!response.ok) {
+        setError("There was an error adding the item to the cart: " + response.statusText)
+      } else {
+      const newlyCreatedItem = await response.json() //this will have an id on it 
+      //make the change on the front end
+      setCartItems([...cartItems, newlyCreatedItem])
+      }
+    } catch (error: any) {
+      setError("There was an error adding the item to the cart: " + error.message)
+    }
+    setIsAddingToCart(false)
+  }
+  return (
+    <div className="d-flex flex-wrap gap-3">
+      {isLoading && <p className="text-body-tertiary">Loading...</p>}
+      {error && <p className="text-danger">{error}</p>}
+      {!isLoading && !error && games.length === 0 && <p className="text-body-tertiary">No games found</p>}
+      {!isLoading && !error && games.length > 0 && <h2 className="display-5 mb-4">Check Out Our Games!</h2>}
+      {games.map(game => (
+        <div key={game.id} className="card flex-grow-1">
+          <h3 className="card-title">{game.title}</h3>
+          <p className="card-text">{game.genre}</p>
+          <button className="btn btn-primary"
+          disabled={isAddingToCart}
+            onClick={() => addToCart(game.id)}
+          >
+            {isAddingToCart ? "Adding..." : "$" + game.price.toFixed(2)}
+          </button>
+        </div>
+      ))}
+    </div>
+  )
+}
